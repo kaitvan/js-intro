f90568081ba195512c2ee3ec986d837fbc04c5a4
kaitvan <kaitlyn.vanhook@gmail.com>
2020-07-20 18:51:57 -0500

    tested data types

--- main.js
+++ main.js
@@ -1,1 +1,66 @@
-console.log("hey javascript")
+// ********** UNDEFINED -- declared a variable, but it is not assigned a value
+
+// let x;
+// console.log(x) // => undefined
+
+// console.log(y) // => not defined--it has not been declared in the code base
+
+// ********** BOOLEAN -- true or false
+
+// let x = true;
+// let y = false;
+
+// console.log(x, y)
+
+// if (!x) {
+//     console.log("This is true.")
+// } else {
+//     console.log("This is false.")
+// } 
+
+// ********** NUMBER
+
+// let number = '22';
+
+// if users are entering information in a form, it will be returned as a string
+
+// console.log(number * 5) // => 110 js converts it to a number
+// console.log(number + 5) // => 225 js converts it to a string--thinks concatenation
+// console.log(Number(number) + 5) // => 27 using number method converts to a number instead of string
+
+// ********** STRING
+
+// let string = "Today is Monday!"
+
+// console.log(typeof string) // => returns string
+
+// if (string === "Today is Monday!") {
+//     console.log("YAYAYYA!")
+// }
+
+// *** ARRAYS *** 
+
+let array = []
+console.log(typeof array)
+
+// *** OBJECTS ***
+
+let name = "Kaitlyn",
+    age = 30,
+    school = "NSS";
+
+let object = {
+    name,
+    age,
+    school
+}
+
+console.log(object.name)
+
+// *** FUNCTIONS ***
+
+// let myFunction = () => {
+//     return
+// }
+
+// console.log(typeof myFunction)
